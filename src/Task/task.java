3d7ebfd1d7adcfe9b91cf36876746f30c33f3db2
spowers0409 <therealsjpowers04@gmail.com>
2022-01-25 09:00:06 -0600

    Added comments

--- src/Task/task.java
+++ src/Task/task.java
@@ -6,12 +6,15 @@ public class task {
 	private String description;		// No longer than 50
 	
 	public task(String taskID, String name, String description) {
+		// verifies that taskID will not be null or longer than 10 characters
 		if(taskID == null || taskID.length() > 10) {
 			throw new IllegalArgumentException("Invalid");
 		}
+		// verifies that name will not be null or longer than 20 characters
 		if(name == null || name.length() > 20) {
 			throw new IllegalArgumentException("Invalid");
 		}
+		// verifies that description will not be null or longer than 50 characters
 		if(description == null || description.length() > 50) {
 			throw new IllegalArgumentException("Invalid");
 		}
@@ -21,6 +24,7 @@ public class task {
 		this.description = description;
 	}
 	
+	// Getters and setters
 	public String getTaskID() {
 		return taskID;
 	}
--- src/Task/taskService.java
+++ src/Task/taskService.java
@@ -5,10 +5,12 @@ import java.util.*;
 public class taskService {
 	private final List<task> listOfTask = new ArrayList<>();
 	
+	// Create a unique ID number of which is not longer than 10
 	public String newTaskID() {
 		return UUID.randomUUID().toString().substring(0, Math.min(toString().length(), 10));
 	}
 	
+	// Allow to search ID numbers otherwise throw an exception
 	private task searchForTask(String taskID) throws Exception {
 		int i = 0;
 		
@@ -21,18 +23,20 @@ public class taskService {
 		throw new Exception("No ID");
 	}
 	
+	// Add new task with new name and description
 	public void addTask(String name, String description) {
 		task task = new task(newTaskID(), name, description);
 		listOfTask.add(task);
 	}
+	// Delete task by ID number
 	public void deleteTask(String taskID) throws Exception {
 		listOfTask.remove(searchForTask(taskID));
 	}
-	
+	// Update name of taskID
 	public void updateName(String taskID, String name) throws Exception {
 		searchForTask(taskID).setName(name);
 	}
-	
+	// Update description of taskID
 	public void updateDescription(String taskID, String description) throws Exception {
 		searchForTask(taskID).setDescription(description);
 	}
